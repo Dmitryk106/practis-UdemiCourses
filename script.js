701d4b70f069ead5bf7eb1fa31279c7bde198a67
Dmityk106 <dkonchik@gmail.com>
2021-09-18 15:27:31 +0300

    15 lesson

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 8);
 let personalMovieDB = {
     count : numberOfFilms,
@@ -10,10 +10,19 @@ let personalMovieDB = {
 
 for (let i = 0; i < 2; i++){
 
-let quest1 = prompt('Один из последних просмотренных фильмов?');
-let quest2 = prompt('На сколько оцените его?');
-
-personalMovieDB.movies[quest1] = quest2; 
+let a = prompt('Один из последних просмотренных фильмов?');
+let b = prompt('На сколько оцените его?');
+if (a == '' || a == undefined || a.length > 50 || b == '' || b == undefined )
+{
+    i--;
+} else { personalMovieDB.movies[a] = b;}
 }
 
-console.log(personalMovieDB)
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 30) {
+    alert ('Вы киноман');
+} else { alert ('Вы классический зритель');}
+
+
+console.log(personalMovieDB);
